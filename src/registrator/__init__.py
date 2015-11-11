4c5c85227f2d17dd6a79f5730b2b334228e1e35f
Leif Johansson <leifj@sunet.se>
2015-11-11 13:38:32 +0100

    initialize backend state on startup

--- src/registrator/__init__.py
+++ src/registrator/__init__.py
@@ -13,12 +13,18 @@ def main():
 
     docker_client = docker.Client(base_url='unix://var/run/docker.sock')
 
+    def _do_status(status, info):
+        for backend in backends:
+            if hasattr(backend, status):
+                cb = getattr(backend, status)
+                if cb and hasattr(cb,'__call__'):
+                    cb(info)
+
+    for container in docker_client.containers(filters=dict(status='running')):
+        _do_status('running', container)
+
     for event in docker_client.events():
         if 'status' in event:
             status = event['status']
             info = docker_client.inspect_container(container=event['container'])
-            for backend in backends:
-                if hasattr(backend, status):
-                    cb = getattr(backend, status)
-                    if cb and hasattr(cb,'__call__'):
-                        cb(info)
+            _do_status(status, info)
