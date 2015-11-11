f8b05fef6c7b1ff3668ae4765113bc4fb1aa8d74
Leif Johansson <leifj@sunet.se>
2015-11-11 13:38:58 +0100

    a sample monitor

--- src/registrator/builtins.py
+++ src/registrator/builtins.py
@@ -1,6 +1,7 @@
 import os
 import etcd
-from backends import backend
+from backends import backend, monitor
+from time import sleep
 
 
 @backend(name="etcd")
@@ -19,3 +20,28 @@ class Log(object):
 
     def __call__(self, *args, **kwargs):
         print args[0]
+
+
+@backend(name="monitor_test")
+class TestMonitor(object):
+
+    def __init__(self):
+        self.threads = dict()
+        self.done = False
+
+    def _mon(self, info):
+        self.done = False
+        while not self.done:
+            sleep(10)
+            print info
+
+    def start(self, info):
+        if 'container' in info:
+            t = monitor(self._mon, info)
+            self.threads[info['container']] = t
+
+    def stop(self, info):
+        if 'container' in info and info['container'] in self.threads:
+            t = self.threads[info['container']]
+            self.done = True
+            t.join()
